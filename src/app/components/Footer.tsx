7f31520556ff8fa699ea63a58d52b8ebad6901d2
Abhinav Anand <abhinav@Abhinavs-MacBook-Air.local>
2024-08-21 10:11:33 +0530

    Navbar initial design

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import logo from "@/assets/logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bottom-0 fixed text-gray-600 body-font">
-  <div className=" px-4 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl">Durga Engineers</span>
-    </a>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 Durga Engineers </p>
-  </div>
-</footer>
-  )
-}
+    <footer className="bottom-0 fixed w-full text-gray-600 body-font bg-white">
+      <div className="px-4 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <Image
+            className="h-10 w-10" // Adjusted size for better alignment
+            src={logo}
+            quality={100}
+            height={40}
+            width={40}
+            alt="Durga Engineers"
+          />
+          <span className="ml-3 text-xl">Durga Engineers</span>
+        </a>
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 text-center">
+          © 2024 Durga Engineers
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,34 +1,186 @@
-import React from 'react';
-import Image from 'next/image';
-import logo from '@/assets/logo.png';
+"use client";
+import { Fragment, useState } from "react";
+import { Dialog, Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
-const NavBar = () => {
-  return (
-    <header className="text-gray-600 body-font">
-  <div className=" mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <Image src={logo} className='h-50 w-50' alt='Logo' height={80} width={80} quality={100}/>
-      {/* <span className="ml-3 text-xl font-">DURGA ENGINEERS</span> */}
-      
-      <h1 className="ml-3 text-2xl font-semibold text-gray-900 tracking-wide">
-  Durga Engineers
-</h1>
-    </a>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center md:mr-10">
-      <a className="mr-5 hover:text-gray-900">Home</a>
-      <a className="mr-5 hover:text-gray-900">About Us</a>
-      <a className="mr-5 hover:text-gray-900">Products & Services</a>
-      <a className="mr-5 hover:text-gray-900">Milestones</a>
-      <button className="text-gray-900 bg-white border-2 py-2 px-5 focus:outline-none hover:bg-red-500 hover:text-white rounded-full text-lg">Contact Us</button>
-    </nav>
-    {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button> */}
-  </div>
-</header>
-  )
+const pages = [
+  { name: "Home", href: "/" },
+  { name: "About us", href: "/about" },
+  { name: "Products & Services", href: "/products" },
+  { name: "Milestones", href: "/milestones" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export default NavBar
+export default function Example() {
+  const path = usePathname();
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="bg-white">
+      {/* Mobile menu */}
+      {/* Mobile menu */}
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-40 flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <Dialog.Panel className="relative flex w-full flex-col overflow-y-auto bg-red-100 pb-12 shadow-xl">
+                <div className="flex px-4 pb-2 pt-5 ml-auto">
+                  <button
+                    type="button"
+                    className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                    onClick={() => setOpen(false)}
+                  >
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-8 w-8" aria-hidden="true" />
+                  </button>
+                </div>
+
+                {/* Logo and title */}
+                <div className="flex">
+                  <Image
+                    className="h-20 w-22 mt-10"
+                    src={logo}
+                    quality={100}
+                    height={100}
+                    width={100}
+                    alt="Durga Engineers"
+                  />
+                  <div className="mt-14 ml-2 font-mono">
+                    <h4 className="text-2xl text-gray-900">
+                      Durga Electronics
+                    </h4>
+                    <p className="text-sm text-indigo-600">
+                      {/* Software Engineer @Oracle */}
+                    </p>
+                  </div>
+                </div>
+
+                {/* Navigation links */}
+                <div className="space-y-6 border-t border-gray-200 px-4 py-2 border-b mt-4">
+                  {pages.map((page) => (
+                    <div key={page.name} className="flow-root">
+                      <Link
+                        href={page.href}
+                        onClick={() => {
+                          setOpen(false);
+                        }}
+                        className={classNames(
+                          page.href === path
+                            ? "border-indigo-600 text-indigo-600"
+                            : "border-transparent text-gray-700 hover:text-gray-800",
+                          "flex items-center text-sm font-medium relative z-10 "
+                        )}
+                      >
+                        {page.name}
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+
+                {/* Contact Us Button for Mobile */}
+                <div className="px-4 py-6">
+                  <Link href="/contact">
+                    <button className="w-full px-4 py-2 text-sm font-medium text-white bg-red-500 hover:bg-red-600 rounded-full focus:outline-none transition">
+                      Contact Us
+                    </button>
+                  </Link>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+      <header className="relative bg-white">
+        <nav
+          aria-label="Top"
+          className="mx-auto max-w-full px-4 sm:px-6 lg:px-8"
+        >
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <button
+                  type="button"
+                  className="relative rounded-md bg-white p-2 text-gray-400 lg:hidden"
+                  onClick={() => setOpen(true)}
+                >
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open menu</span>
+                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Logo and Durga Electronics */}
+                <div className="flex items-center">
+                  <Image
+                    className="w-auto h-12"
+                    src={logo}
+                    alt="logo"
+                    height={60}
+                    width={60}
+                  />
+                  <span className="ml-4 text-2xl font-semibold text-gray-900">
+                    Durga Electronics
+                  </span>
+                </div>
+              </div>
+
+              {/* Flyout menus */}
+              <Popover.Group className="hidden lg:block lg:self-stretch">
+                <div className="flex h-full space-x-8">
+                  {pages.map((page) => (
+                    <Link
+                      key={page.name}
+                      href={page.href}
+                      className={classNames(
+                        page.href === path
+                          ? "border-indigo-600 text-indigo-600"
+                          : "border-transparent text-gray-700 hover:text-gray-800",
+                        "flex items-center text-sm font-medium relative z-10 border-b-2 transition-all"
+                      )}
+                    >
+                      {page.name}
+                    </Link>
+                  ))}
+                  <Link href="/contact">
+                    <button className="ml-4 px-4 py-2 text-sm font-medium text-white bg-red-500 hover:bg-red-600 p-4 rounded-full mt-3 focus:outline-none transition">
+                      Contact Us
+                    </button>
+                  </Link>
+                </div>
+              </Popover.Group>
+            </div>
+            {/* Single bottom border for active tab */}
+            <div className=" border-gray-300" />
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
